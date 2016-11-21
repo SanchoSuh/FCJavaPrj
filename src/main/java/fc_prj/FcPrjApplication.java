e566a014ea74c31c6b6925de104e944473201bee
Inyong Sancho Suh <inyonge2@gmail.com>
2016-11-21 21:26:54 +0900

    add

---
+++ src/main/java/fc_prj/FcPrjApplication.java
@@ -0,0 +1,12 @@
+package fc_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcPrjApplication.class, args);
+	}
+}
